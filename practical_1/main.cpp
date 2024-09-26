79bd8900a4c1b9e14068a8e7a3964e85dcabbfcc
jacktait8 <114003857+jacktait8@users.noreply.github.com>
2024-09-26 13:40:37 +0100

    upgrading pong game
    making the ball move and ball bounce of the paddles

--- practical_1/main.cpp
+++ practical_1/main.cpp
@@ -9,6 +9,11 @@ const Keyboard::Key controls[4] = {
 	Keyboard::Up, 
 	Keyboard::Down
 };
+Vector2f ballVelocity;
+bool isPlayerServing = false;
+const float velocityMultiplier = 1.1f;
+const float initialVelocityX = 100.f;
+const float initialVelocityY = 60.f;
 const Vector2f paddleSize(25.f, 100.f);
 const float ballRadius = 10.f;
 const int gameWidth = 800;
@@ -29,11 +34,21 @@ void Load(){
 	//set size and origin of ball
     ball.setRadius(ballRadius);
     ball.setOrigin(ballRadius / 2.f , 2.f);
+	
+	Reset();
+	
+}
+
+void Reset(){
 	//reset paddle position 
 	paddles[0].setPosition(Vector2f(paddleOffsetWall + paddleSize.x / 2.f , gameHeight /2.f));
 	paddles[1].setPosition(Vector2f(gameWidth - (paddleOffsetWall + paddleSize.x / 2.f) , gameHeight /2.f));
+	
 	//reset paddle position 
 	ball.setPosition(Vector2f(gameWidth / 2.f, gameHeight / 2.f));
+	
+	//reset ball position 
+	ballVelocity = { ( isPlayerServing ? initialVelocityX : -initialVelocityX), initialVelocityY };
 }
 
 void Update(RenderWindow &window) {
@@ -63,7 +78,52 @@ void Update(RenderWindow &window) {
         direction++;
     }
     paddles[0].move(Vector2f(0.f, direction * paddleSpeed * dt));
+	
+	ball.move(ballVelocity * dt);
+	
+	// check ball collision
+	const float bx = ball.getPosition().x;
+	const float by = ball.getPosition().y;
+	
+	if(by > gameHeight ) {
+		ballVelocity.x *= velocityMultiplier;
+		ballVelocity.y *= -velocityMultiplier;
+		ball.move(Vector2(0.f, -10.f));
+	} else if( by < 0) {
+		ballVelocity.x *= velocityMultiplier;
+		ballVelocity.y *= -velocityMultiplier;
+		ball.move(Vector2(0.f, 10.f));	
+	} else if (bx > gameWidth) {
+		// right wall
+		Reset();
+	} else if (bx < 0) {
+		// left wall
+		Reset();
+	}else if (
+		//ball is inline or behind paddle AND
+		bx < paddleSize.x + paddleOffsetWall && 
+		//ball is below top edge of paddle AND
+		by > paddles[0].getPosition().y - (paddleSize.y * 0.5) &&
+		//ball is above bottom edge of paddle
+		by < paddles[0].getPosition().y + (paddleSize.y * 0.5)){
+		// bounce off left paddle
+		ballVelocity.x = -velocityMultiplier;
+		ball.move(Vector2(0.f, 10.f));
+		
+	} else if (		
+		//ball is inline or behind paddle AND
+		bx < paddleSize.x + paddleOffsetWall && 
+		//ball is below top edge of paddle AND
+		by > paddles[0].getPosition().y - (paddleSize.y * 0.5) &&
+		//ball is above bottom edge of paddle
+		by < paddles[0].getPosition().y + (paddleSize.y * 0.5)){
+		//bounces off right paddle
+		ballVelocity.x = -velocityMultiplier;
+		ball.move(Vector2(0.f, 10.f));
+
+	}
 }
+
 
 void Render(RenderWindow &window) {
     // Draw Everything
