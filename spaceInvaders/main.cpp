4b9a756a3e415d9b70804e490c13d3457c7aa1c0
jacktait8 <114003857+jacktait8@users.noreply.github.com>
2024-10-03 18:43:26 +0100

    creating the ship

--- spaceInvaders/main.cpp
+++ spaceInvaders/main.cpp
@@ -1,20 +1,50 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <filesystem>
 
-int main(){
-  sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
-  sf::CircleShape shape(100.f);
-  shape.setFillColor(sf::Color::Green);
+//Main.cpp
+sf::Texture spritesheet;
+sf::Sprite invader;
 
-  while (window.isOpen()){
-      sf::Event event;
-      while (window.pollEvent(event)){
-      if (event.type == sf::Event::Closed){
-        window.close();
-      }
-    }
-    window.clear();
-    window.draw(shape);
-    window.display();
-  }
-  return 0;
+using namespace std;
+using namespace sf;
+
+void Load() {
+
+	if (!spritesheet.loadFromFile("../../../../res/img/invaders_sheet.png")) {
+		cerr << "Failed to load spritesheet!" << std::endl;
+	}
+	invader.setTexture(spritesheet);
+	invader.setTextureRect(IntRect(Vector2i(0, 0), Vector2i(32, 32)));
 }
+
+void Update(RenderWindow& window) {
+	// Reset clock, recalculate deltatime
+	static Clock clock;
+	float dt = clock.restart().asSeconds();
+	// check and consume events
+	Event event;
+	while (window.pollEvent(event)) {
+		if (event.type == Event::Closed) {
+			window.close();
+			return;
+		}
+	}
+}	
+void Render(RenderWindow& window) {
+	
+	window.draw(invader);
+}
+
+int main() {
+	RenderWindow window(VideoMode(500, 500), "PONG");
+	Load();
+	while (window.isOpen()) {
+		window.clear();
+		Update(window);
+		Render(window);
+		window.display();
+	}
+	return 0;
+}
+	
---
+++ spaceInvaders/ship.cpp
@@ -0,0 +1,4 @@
+//ship.h
+#pragma once 
+
+#include 
